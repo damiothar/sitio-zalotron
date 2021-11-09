4b21af1e836347b1ee00ed395b6f72546bd5856d
damiothar <damiothar@users.noreply.github.com>
2021-11-09 08:07:21 +0000

    deploy: 752744f551a9d48adecb00178d5ab80f4b7b6b55

---
+++ _nuxt/static/1636445240/portfolio/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsitio-zalotron\u002F_nuxt\u002Fstatic\u002F1636445240",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio",config:{_app:{basePath:"\u002Fsitio-zalotron\u002F",assetsPath:"\u002Fsitio-zalotron\u002F_nuxt\u002F",cdnURL:a}}}}(null));
