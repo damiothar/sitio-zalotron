6e6a3d6b0a834a9717ab511c9972ab730c1c49a2
damiothar <damiothar@users.noreply.github.com>
2021-11-14 20:13:31 +0000

    deploy: 3146294a1db1ca52beda4d3e1040466d2f1b4b60

---
+++ _nuxt/9d426d9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{402:function(n,t,e){"use strict";e.r(t);var c={asyncData:function(n){n.params,n.store}},l=e(37),component=Object(l.a)(c,(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",[n._v(n._s(n.slug))])}),[],!1,null,null,null);t.default=component.exports}}]);
