48cb3adb085c5767f508f1c00310ea1fcdd15521
Damian Othar <damianothar@gmail.com>
2021-11-16 00:43:54 -0300

    fix zalo branch

---
+++ mixins/materials.js
@@ -0,0 +1,22 @@
+import * as THREE from 'three/build/three.module';
+
+export default {
+	data() {
+		return {};
+	},
+	methods: {
+		createMaterials() {
+			const THAT = this;
+
+			this.matWhite = new THREE.MeshStandardMaterial({ color: 0xffffff });
+
+			this.matReflective = new THREE.MeshPhysicalMaterial({
+				color: 0xffffff,
+				roughness: 0,
+				reflectivity: 1,
+				envMap: THAT.envMat,
+				envMapIntensity: 2,
+			});
+		},
+	},
+};
